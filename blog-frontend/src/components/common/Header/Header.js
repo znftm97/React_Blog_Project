ab3a175f2b0f05fe3f8f20d69cdc3c5e6e544f03
jihoon <znftm97@naver.com>
2020-08-23 19:25:02 +0900

    Button Create

--- blog-frontend/src/components/common/Header/Header.js
+++ blog-frontend/src/components/common/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Header.scss';
 import classNames from 'classnames/bind';
 import {Link} from 'react-router-dom';
+import Button from 'components/common/Button'
 
 const cx = classNames.bind(styles);
 
@@ -12,7 +13,7 @@ const Header = () =>(
                 <Link to='/'>ReactBlog</Link>
             </div>
             <div className={cx('right')}>
-                right
+                <Button theme="outline" to="/editor">New Post</Button>
             </div>
         </div>
     </header>
--- blog-frontend/src/components/common/PageTemplate/PageTemplate.js
+++ blog-frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -7,12 +7,16 @@ import Footer from '../Footer/Footer';
 
 const cx = classNames.bind(styles);
 
+/*
+함수형 컴포넌트, children은 ListPage.js에서 PageTmplate 컴포넌트 안에
+컴포넌트들을 의미
+*/
 const PageTemplate = ({children}) => (
     <div className={cx('page-template')}>
         <Header/>
-        <main>
-            {children}
-        </main>
+            <main>
+                {children}
+            </main>
         <Footer/>
     </div>
     

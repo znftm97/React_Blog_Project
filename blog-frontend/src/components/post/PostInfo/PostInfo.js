856bddc04401eed1617ce20074e3431ef0704c7a
jihoon <znftm97@naver.com>
2020-08-23 20:48:03 +0900

    PostList && PostPage UI Implements

---
+++ blog-frontend/src/components/post/PostInfo/PostInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './PostInfo.scss'
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const PostInfo = () => (
+    <div className={cx('post-info')}>
+        <div className={cx('info')}>
+            <h1>Title</h1>
+            <div className={cx('tags')}>
+                <a>#tag</a><a>#tag</a><a>#tag</a>
+            </div>
+            <div className={cx('date')}>date</div>
+        </div>
+    </div>
+)
+
+export default PostInfo;

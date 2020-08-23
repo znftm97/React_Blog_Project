55a4d356dc337b3c0b56e0faa77eb3458f21c91e
jihoon <znftm97@naver.com>
2020-08-23 16:13:17 +0900

    Header Component Create

---
+++ blog-frontend/src/components/common/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Header.scss';
+import classNames from 'classnames/bind';
+import {Link} from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+const Header = () =>(
+    <header className={cx('header')}>
+        <div className={cx('header-content')}>
+            <div className={cx('brand')}>
+                <Link to='/'>ReactBlog</Link>
+            </div>
+            <div className={cx('right')}>
+                right
+            </div>
+        </div>
+    </header>
+)
+
+export default Header;
